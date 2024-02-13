f457750ebd305ace7d5ebb044eb80e609030b1e0
Philipp <sabelnickov.filipp@yandex.ru>
2024-02-13 22:23:50 +0500

    add src/

---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log("Hello, Hexlet!");
+import _ from "lodash";
+console.log(_.last(['one', 'two']));
+
+export default getBirthYear = (age) => {
+	const birthYear = 2001;
+
+	return birthYear;
+
+};
